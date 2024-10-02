00080db4c25e43d06ba5b6613ed78647ee7d7b93
LisetteMurugi <lisettendwiga4@gmail.com>
2024-10-02 21:45:14 +0300

    java

--- 0x00-ES6_basic/tests/2-then.js
+++
@@ -1,7 +0,0 @@
-export default function handleResponseFromAPI(promise) {
-  const p = { status: 200, body: 'success' };
-  return promise
-    .then(() => p)
-    .catch(() => new Error())
-    .finally(() => { console.log('Got a response from the API'); });
-}
--- 0x00-ES6_basic/tests/README.md
+++
@@ -1,3 +0,0 @@
-ES6 Promises
-Learning Objectives
-Promises (how, why, and what) How to use the then, resolve, catch methods 
